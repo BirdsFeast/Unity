0669fcc920c80ddbfdbc6690086cc6435f618e40
Yasu <carlosyasu91@gmail.com>
2018-03-24 19:14:36 -0700

    Added CreepAMovement, WaveAPath classes, Created Worm and Ground prefab

---
+++ Bird_feast/Assets/CreepAMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreepAMovement : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private int waypointsIndex = 0;
+
+	void Start() {
+		target = WaveAPath.points [0];
+	}
+
+	void Update () {
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime);
+		if (Vector3.Distance (transform.position, target.position) < 0.4f) {
+			GetNextWaypoint ();
+		}
+	}
+
+	void GetNextWaypoint() {
+		waypointsIndex++;
+		if (waypointsIndex >= WaveAPath.points.Length) return;
+		target = WaveAPath.points [waypointsIndex];
+	}
+}
