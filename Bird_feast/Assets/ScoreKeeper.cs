bfe9c92a1b9c0f42f48c109e30ac1ac880717117
Yasu <carlosyasu91@gmail.com>
2018-03-25 19:44:14 -0700

    Create ScoreKeeper class and add UI elements to scene

---
+++ Bird_feast/Assets/ScoreKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	[Header("Attributes")]
+	public static int wormsScore = 0;
+	public static int birdsScore = 0;
+
+	[Header("Unity Setup Fields")]
+	public Text wormsText;
+	public Text birdsText;
+
+	void Update() {
+		UpdateUI ();
+	}
+
+	void UpdateUI() {
+		wormsText.text = "Worms: " + wormsScore.ToString();
+		birdsText.text = "Birds: " + birdsScore.ToString();
+	}
+}
