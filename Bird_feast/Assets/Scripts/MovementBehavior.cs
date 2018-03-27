f35762c137dfbf88dbc6ac8af735a04e4cfb82e5
Yasu <carlosyasu91@gmail.com>
2018-03-26 22:09:46 -0700

    Add box that moves around the ground

---
+++ Bird_feast/Assets/Scripts/MovementBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehavior : MonoBehaviour {
+
+	private float speed = 100f;
+
+	void Update() {
+		float moveHorizontal = GetMovementHorizontal ();
+		float moveVertical = GetMovementVertical ();
+		transform.Translate (moveHorizontal, 0f, moveVertical, Space.World);
+	}
+
+	float GetMovementHorizontal () {
+		Debug.Log (speed);
+		return Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+	}
+
+	float GetMovementVertical() {
+		return Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+	}
+}
