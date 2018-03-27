a9fa54a1a12c439903ce2c76db850cc29fb410f7
Yasu <carlosyasu91@gmail.com>
2018-03-26 22:18:55 -0700

    Make birds destroy worms

---
+++ Bird_feast/Assets/Scripts/CollisionBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBehavior : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.CompareTag ("Enemy")) {
+			Destroy (collision.gameObject);
+			ScoreKeeper.birdsScore++;
+		}
+	}
+}
