77960a08c4fe49c8f7a19fcb9541b97cc045a442
Yasu <carlosyasu91@gmail.com>
2018-04-01 18:39:03 -0700

    Add cooldown indicators

--- Bird_feast/Assets/Birds/Scripts/MovementBehavior.cs
+++ Bird_feast/Assets/Birds/Scripts/MovementBehavior.cs
@@ -4,31 +4,46 @@ using UnityEngine;
 
 public class MovementBehavior : MonoBehaviour {
 
-	private float speed = 20f;
+	private float movementSpeed = 30f;
+  private Vector3 target;
+  private Vector3 direction;
+  private float targetRadiusLimit = 3f;
+  private bool debouceTargetCreation = false;
+
+  public GameObject targetMarker;
 
 	void Update() {
-		float moveHorizontal = GetMovementHorizontal ();
-		float moveVertical = GetMovementVertical ();
-		transform.Translate (moveHorizontal, 0f, moveVertical, Space.World);
+    if (Input.GetMouseButtonDown (1)) {
+      OnRightClick();
+    }
+    Move();
 	}
 
-	float GetMovementHorizontal () {
-		if (Input.GetAxis ("Horizontal") > 0f) {
-			return Time.deltaTime* speed;
-		}
-		if (Input.GetAxis ("Horizontal") < 0f) {
-			return -1 * Time.deltaTime * speed;
-		}
-		return 0f;
-	}
+  void OnRightClick () {
+    SetTarget ();
+  }
 
-	float GetMovementVertical() {
-		if (Input.GetAxis ("Vertical") > 0f) {
-			return Time.deltaTime* speed;
-		}
-		if (Input.GetAxis ("Vertical") < 0f) {
-			return -1 * Time.deltaTime * speed;
-		}
-		return 0f;
-	}
+  void SetTarget() {
+    Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+    if (Physics.Raycast(clickRay, out hit)) {
+      target = hit.point;
+      Quaternion defaultRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+      Instantiate(targetMarker, target, defaultRotation);
+      direction = transform.position - target;
+      debouceTargetCreation = true;
+    }
+  }
+
+  void Move() {
+    if(IsUnitOnTarget()) return;
+    float translationX = direction.normalized.x * Time.deltaTime * movementSpeed;
+    float translationZ = direction.normalized.z * Time.deltaTime * movementSpeed;
+    transform.Translate(-1f * translationX, 0f, -1f * translationZ, Space.World);
+  }
+
+  bool IsUnitOnTarget () {
+    return Vector3.Distance(transform.position, target) < targetRadiusLimit;
+  }
+
 }
---
+++ Bird_feast/Assets/MoveMarker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMarker : MonoBehaviour {
+
+  private float lifetime = 3f;
+  private float currentLifetime = 0f;
+
+
+  void Start() {
+    currentLifetime = lifetime;
+  }
+
+	void Update ()
+  {
+    currentLifetime -= Time.deltaTime;
+    if (currentLifetime <= 0f) {
+      Destroy(gameObject);
+    }
+	}
+}
